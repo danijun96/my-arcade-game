ee34da603080af31b8aa8847669a001c8e37b69d
Daniel96 <danielmahdievzandi@gmail.com>
2018-06-13 23:58:53 +0300

    convert object to es6

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    constructor (){
     this.x = Math.random() * 505;
     this.y = 63 + (Math.round(Math.random() * 2) * 83);
 
@@ -10,11 +11,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+    update (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -23,8 +22,25 @@ Enemy.prototype.update = function(dt) {
     if (this.x >= 505) {
         this.y = 63 + (Math.round(Math.random() * 2) * 83);
         this.x = -50;
+
     }
+
 }
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//         // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+//     this.x += this.speed * dt;
+
+//     if (this.x >= 505) {
+//         this.y = 63 + (Math.round(Math.random() * 2) * 83);
+//         this.x = -50;
+
+//     }
+// }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
