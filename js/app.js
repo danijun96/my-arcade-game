c8afb8b3747a01206cd3ff1a7185a1fe6e92519c
Daniel96 <danielmahdievzandi@gmail.com>
2018-06-14 00:26:10 +0300

    more challenges

--- js/app.js
+++ js/app.js
@@ -106,7 +106,7 @@ for (var addEnemies = 0; addEnemies < 4; addEnemies++) {
     allEnemies.push(enemy);
     if(this.score >= 10 <= 15){
         addEnemies++;
-        this.speed = (Math.random() * 250) + 95;
+        this.speed = (Math.random() * 550) + 295;
     }
 };
 
